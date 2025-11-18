319ebb42fea05f783c2e1176323a40303a40107e
azinfonoon <af2580311653>
2025-11-18 11:42:08 +0330

    Add Create Action

---
+++ BlogWithCommentEditorASPMVC/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWithCommentEditorASPMVC.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Authorize(Roles = "admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BlogWithCommentEditorASPMVC/Areas/Admin/Models/Dtos/Blog/CreateBlogDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogWithCommentEditorASPMVC.Areas.Admin.Models.Dtos.Blog
+{
+    public class CreateBlogDto
+    {
+        [Required]
+        [MaxLength(500)]
+        public required string Title { get; set; }
+
+        [Required]
+        public required string Content { get; set; }
+
+    }
+}
